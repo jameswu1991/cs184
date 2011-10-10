97227f515c67b55cc5af99259699a15a719a35c5
Connie Guo <connie.guo107@gmail.com>
2011-10-10 12:38:51 -0700

    added freeimage

--- as4/main.cpp
+++ as4/main.cpp
@@ -1,5 +1,8 @@
 #include <vector>
 #include <iostream>
+#include <iostream>
+#include "FreeImage.h"
+
 #include <math.h>
 
 #include "window.h"
@@ -12,12 +15,42 @@
 #include "model.h"
 #include "scene.h"
 
+#define WIDTH 100
+#define HEIGHT 100
+#define BPP 24
+
+using namespace std;
+
 Parser parser;
 Rendering rendering;
 Scene scene;
 
 int main(int argc, char *argv[]) {
 
+	FreeImage_Initialise();
+	
+	FIBITMAP* bitmap = FreeImage_Allocate(WIDTH, HEIGHT, BPP);
+	RGBQUAD color;
+	
+	if (!bitmap)
+		exit (1);
+	
+	//Draws a gradient from blue to green;
+	for (int i=0; i<WIDTH; i++) {
+		for (int j=0; j<HEIGHT; j++) {
+			color.rgbRed = 0;
+			color.rgbGreen = (double) i / WIDTH * 255.0;
+			color.rgbBlue = (double) j / HEIGHT * 255.0;
+			FreeImage_SetPixelColor(bitmap, i, j, &color);
+		}
+	}
+	
+	if (FreeImage_Save(FIF_PNG, bitmap, "test.png", 0))
+		cout << "Image_successfully_saved!" <<endl;
+	
+	FreeImage_DeInitialise(); 
+
+/*
 	Window *window = Window::get();
 	window->initialize(argc, argv);
 	
@@ -25,11 +58,13 @@ int main(int argc, char *argv[]) {
 	scene.addModel(m);
 	scene.addDirectionalLight(-0.5, -1, 0);
 	scene.addDirectionalLight(-0.1, -0.1, 0.1);
-	// Sphere s = Sphere(center, 0.5);
-	// scene.addSphere(s);
+	Vertex center = Vertex(0, 0, 1);
+	Sphere s = Sphere(center, 0.1);
+	scene.addSphere(s);
 	rendering.render(scene, window);
 	
 	window->show();
 	
 	return 0;
+*/
 }
