63c14c61ce4be34d6a987194685a7662b2a13d58
Connie Guo <connie.guo107@gmail.com>
2011-11-29 21:49:14 -0800

    patch

---
+++ final/patch.cpp
@@ -0,0 +1,8 @@
+#include "patch.h"
+
+using namespace std;
+using namespace Eigen;
+
+Patch::Patch(vector<Vector3f> vertices, vector<float> color, float reflectance, float irradiance, map<Patch, float> viewFactors){
+	
+}
---
+++ final/patch.h
@@ -0,0 +1,23 @@
+#ifndef patch_h
+#define patch_h
+
+#include <vector>
+#include <iostream>
+#include <math.h>
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <map>
+
+using namespace std;
+using namespace Eigen;
+
+class Patch {
+public:
+	vector<Vector3f> vertices;
+	vector<float> color;
+	float reflectance;
+	float irradiance;
+	map<Patch, float> viewFactors;
+public:
+	Patch(vector<Vector3f> vertices, vector<float> color, float reflectance, float irradiance, map<Patch, float> viewFactors);
+}
