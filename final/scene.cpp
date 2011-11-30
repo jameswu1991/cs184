4faa9b625f0835aaea1e0fb3370082e3ecdaa660
Connie Guo <connie.guo107@gmail.com>
2011-11-30 13:58:25 -0800

    validated subdivide function

--- final/scene.cpp
+++ final/scene.cpp
@@ -11,14 +11,21 @@ vector<float> getVector(float r, float g, float b) {
 
 Scene::Scene() {
 	vector<Vector3f> array(4);
-
+	
+	// test
+	array[0]=Vector3f(0, 1, 0);
+	array[1]=Vector3f(1, 1, 0);
+	array[2]=Vector3f(1, 0, 0);
+	array[3]=Vector3f(0, 0, 0);
+	patches.push_back(Patch(array, getVector(0.5, 0.5, 0.5), 0.5, 0));
+		
+	/*
 	// floor
 	array[0]=Vector3f(552.8, 0, 0);
 	array[1]=Vector3f(0, 0, 0);
 	array[2]=Vector3f(0, 0, 559.2);
 	array[3]=Vector3f(549.6, 0, 559.2);
 	patches.push_back(Patch(array, getVector(0.5, 0.5, 0.5), 0.5, 0));
-	
 	// light
 	array[0]=Vector3f(343, 548.8, 227);
 	array[1]=Vector3f(343, 548.8, 332);
@@ -107,23 +114,42 @@ Scene::Scene() {
 	array[2]=Vector3f(423.0, 330.0, 247.0);
 	array[3]=Vector3f(423.0,   0.0, 247.0);
 	patches.push_back(Patch(array, getVector(0.5, 0.5, 0.5), 0.5, 0));
+	*/
+	
+	// subdivide the original patches
+	subdivideTo16();
+	
+	/*
+	cout << "Patches size is now " << patches.size() << endl;
+	int i;
+	for (i=0; i<patches.size(); i++) {
+		cout << "Top left point for Patch " << i << " is " << patches[i].vertices[0] << endl;
+		cout << "Top right point for Patch " << i << " is " << patches[i].vertices[1] << endl;
+		cout << "Bottom right point for Patch " << i << " is " << patches[i].vertices[2] << endl;
+		cout << "Bottom left point for Patch " << i << " is " << patches[i].vertices[3] << endl;
+	}
+	*/
+	
 }
 
 void Scene::subdivideTo16() {
-	Scene::subdivide(); // 2x2
-	Scene::subdivide(); // 4x4
-	Scene::subdivide(); // 8x8
-	Scene::subdivide(); // 16x16
+	subdivide(); // 2x2
+	subdivide(); // 4x4
+	subdivide(); // 8x8
+	subdivide(); // 16x16
 }
 
 void Scene::subdivide() {
 	vector<Patch> newPatches;
 	int i;
 	for (i=0; i<patches.size(); i++) {
-		// Get the vertices of the current patch
+		
+		// Store the original patch properties.
 		vector<float> origColor = patches[i].color;
 		float origRefl = patches[i].reflectance;
 		float origIrr = patches[i].irradiance;
+		
+		// Get the vertices of the current patch
 		Vector3f topLeft = patches[i].vertices[0];
 		Vector3f topRight = patches[i].vertices[1];
 		Vector3f bottomRight = patches[i].vertices[2];
